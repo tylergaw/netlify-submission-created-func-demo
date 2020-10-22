e76b3c91728fdf224b2163c6b49f06248fd2827e
Tyler Gaw <me@tylergaw.com>
2020-10-22 08:44:25 -0400

    Adds log to function

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -1,4 +1,6 @@
 exports.handler = async () => {
+  console.log("submission-created.js invoked");
+
   return {
     statusCode: 200,
     body: JSON.stringify({ message: "A new form submission came in" }),
